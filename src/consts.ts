a71cd10bd71ca3c98817bb643c83841a32332c48
OlegParamey <olegparamey4@gmail.com>
2025-12-15 18:04:57 +0100

    added .env for URLs

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 export const URL_GET_GENERATION_MIX_FOR_THREE_NEXT_DAYS =
-	'https://energy-consumption-and-charging-app.onrender.com/generation-mix-for-three-next-days';
+	import.meta.env.VITE_URL_GET_GENERATION_MIX_FOR_THREE_NEXT_DAYS ||
+	'http://localhost:3000/generation-mix-for-three-next-days';
 
 export const URL_GET_OPTIMAL_CHARGING_WINDOW_TIME =
-	'https://energy-consumption-and-charging-app.onrender.com/optimal-charging-window-time';
+	import.meta.env.VITE_URL_GET_OPTIMAL_CHARGING_WINDOW_TIME ||
+	'http://localhost:3000/optimal-charging-window-time?hours=2';
